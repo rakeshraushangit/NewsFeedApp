64bd6e52367f156fbbc4d295b0f0a45639b46e7c
Rakesh Kumar Raushan <rkraushan@altametrics.com>
2022-05-26 10:33:21 +0530

    Initial Commit

---
+++ NewsFeedApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//
+//	ContentView.swift
+//	NewsFeedApp
+//
+//	Created By Altametrics on 5/26/22
+//	
+//
+
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsFeedApp/NewsFeedAppApp.swift
@@ -0,0 +1,21 @@
+//
+//
+//	NewsFeedAppApp.swift
+//	NewsFeedApp
+//
+//	Created By Altametrics on 5/26/22
+//	
+//
+
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
